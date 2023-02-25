d7b84095dc766e922b8a50559179c1536a4e53d2
Mohamed Zribi <86979984+MohamedZribi@users.noreply.github.com>
2023-02-25 13:05:45 +0100

    Add operate function
    Calculator now operates with success

--- script.js
+++ script.js
@@ -1,50 +1,121 @@
 
-// I guess I need to define operator, a and b from the 
-// document
 
-// The display is also needed
-
-// Starting from here
-
-function operate(operator, a, b){
-    if(operator=== '+'){
-        return (a + b)
-    }
-    else if (operator === '-'){
-        return (a - b)
-    }
-    else if (operator === '*'){
-        return (a * b)
-    }
-    else if (operator === '/') {
-        return (a / b)
-    }
-
+// The clear function
+function  clear(){
+    numArray = [];
+    return document.getElementById("display").innerHTML = ""
 };
 
-
-
-/* Create the functions that populate the display 
-    when you click the number buttons.
-    You should be storing the ‘display value’ 
-    in a variable somewhere for use in the next step. */
-
-
+// Attach clear() to its button
+const clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', (e) => {
+    clear()
+})
 
 
 
-
-
-
-
-
+// Each time a number is clicked it's entered in the array
+let numArray = [];
 const numbers = document.querySelectorAll('.number')
-console.log(numbers)
-
 for (const number of numbers) {
     number.addEventListener('click', (e) => {
-        console.log(number.innerHTML)
-        document.getElementById("display").innerHTML = number.textContent;
-    
+        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + number.textContent;
+
+        console.log(numArray.length)
+
+        if (numArray.length == 0) {
+            numArray.push(number.innerHTML)
+        }
+        else if (numArray.length == 1) {
+            numArray[0] = numArray[0] + number.innerHTML
+        }
+        else if (numArray.length == 2) {
+            numArray[2] =  number.innerHTML 
+        }
+
+        else if (numArray.length == 3) {
+            numArray[2] =  numArray[2] + number.innerHTML 
+
+        }
+
+
+
+        console.log(numArray)
+
     });
 };
+
+
+
+
+// Read the operators and display them
+const operators = document.querySelectorAll('.operator')
+for (const operator of operators){
+    operator.addEventListener('click', (e) => {
+
+        if (numArray.length >= 3 ) {
+            result = operate(numArray)
+            console.log(result)
+            numArray = [];
+            numArray.push(result)
+    
+        };
+
+        document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + " " + operator.textContent + " ";
+        op = operator.innerHTML
+        numArray.push(op)
+
+   
+    
+
+    })
+
+
+};
+
+
+
+
+
+
+function operate(numArray){
+
+    if(op === '+'){
+        result =  (Number(numArray[0]) + Number(numArray[2]))
+    }
+    else if (op === '-'){
+        result =  (Number(numArray[0]) - Number(numArray[2]))
+    }
+    else if (op === '*'){
+        result =  (Number(numArray[0]) * Number(numArray[2]))
+    }
+    else if (op === '/') {
+        result =  (Number(numArray[0]) / Number(numArray[2]))
+    }
+
+    else {
+        result =  "ERROR"
+    } 
+
+    // round the result
+
+    if (result % 1 != 0) {
+        result = Math.round(result * 10) / 10
+    };
+
+    document.getElementById("display").innerHTML = result
+    return result
+
+};
+
+
+
+
+const equalSign = document.querySelector('.equal-sign')
+equalSign.addEventListener('click', (e) => {
+
+    console.log('This is working')
+    console.log(numArray)
+    operate(numArray)
+
+});
